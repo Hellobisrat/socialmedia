be29661301d740e5604f817f54121b204f950d5d
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2026-02-26 10:58:47 -0600

    Remove .env from repo

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,10 @@
 import db from './config/db.js';
+import cloudinary from "./config/cloudinary.js"
 import dotenv from 'dotenv';
 
 
 dotenv.config();
-
+console.log("Cloudinary connected as:", cloudinary.config().cloud_name);
 
 
 db();
