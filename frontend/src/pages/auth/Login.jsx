f05baea9fbf6b4092544793b145e1eadd250f20f
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2026-06-03 10:52:18 -0500

    Login page updated

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import Input from "../../components/ui/Input";
 import Button from "../../components/ui/Button";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
+import { useAuth } from "../../context/AuthContext";
 
 export default function Login() {
-  const navigate = useNavigate()
-  const handleLogin = (e) => {
+  const navigate = useNavigate();
+  const { login } = useAuth();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
     e.preventDefault();
-    navigate('/')
-    toast.success("Logged in!");
+
+    try {
+      await login(email, password);
+      toast.success("Logged in!");
+      navigate("/");
+    } catch (err) {
+      toast.error(err);
+    }
   };
 
   return (
@@ -17,8 +30,22 @@ export default function Login() {
         <h1 className="text-2xl font-bold mb-6 text-center">Welcome Back</h1>
 
         <form onSubmit={handleLogin} className="space-y-4">
-          <Input placeholder="Email" type="email" required />
-          <Input placeholder="Password" type="password" required />
+          <Input
+            placeholder="Email"
+            type="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <Input
+            placeholder="Password"
+            type="password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
           <Button className="w-full">Login</Button>
         </form>
       </div>
